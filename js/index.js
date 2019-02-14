74e2a7ec8af808736d6ea30d25be27d3cd29c1e5
CrypticCoder2502 <43320248+CrypticCoder2502@users.noreply.github.com>
2019-02-14 18:33:13 +0530

    qw12

--- js/index.js
+++
@@ -1,7 +0,0 @@
-var tl = new TimelineMax({repeat: -1}),
-    spidey = document.getElementById("spidey");
-
-tl.to(spidey, 1, {morphSVG:"#avengers"}, "+=1")
-.to(spidey, 1, {morphSVG: "#xmen"}, "+=1")
-.to(spidey, 1, {morphSVG: "#four"}, "+=1")
-.to(spidey, 1, {morphSVG: spidey}, "+=1");
